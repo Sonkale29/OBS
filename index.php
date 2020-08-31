4f3bb7863e659db62271f70a7d151aa8a2c7c4f9
Sonkale29 <sonkale29@gmail.com>
2020-08-31 21:57:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php 
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
 
-phpinfo();
+<?php
+include 'mysqlconfig.php';
 
+$baglanti = @mysql_connect($server, $user, $pass);
+mysql_set_charset('utf8', $baglanti);
+$veritabani = @mysql_select_db($db, $baglanti);
+$fakulteler = mysql_query('SELECT fakulte_adi FROM fakulteler');
+while ($row = mysql_fetch_assoc($fakulteler)) {
+    echo $row['fakulte_adi']."<br>";
+}
 ?>
